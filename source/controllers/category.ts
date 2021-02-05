4616cf7e055c9281397b16c442671c51c0ad4cfc
Dan Örjestad <danorjest@gmail.com>
2021-02-05 11:00:31 +0100

    added get/create

--- source/controllers/category.ts
+++ source/controllers/category.ts
@@ -1,1 +1,38 @@
-export default {};
+import { Request, Response } from 'express';
+import logging from '../config/logging';
+import Category from '../models/category';
+
+const workspace = "category-ctrl"
+
+const getCategories = async (req: Request, res: Response) => {
+    try {
+        let categories = await Category.find();
+        res.status(200).json({ categories: categories });
+    } catch (ex) {
+        logging.error(workspace, "Could not fetch categories", ex.message);
+        res.status(500);
+    }
+};
+
+const newCategory = async (req: Request, res: Response) => {
+    try {
+        const reqCategory = req.body.category;
+        if (!reqCategory) {
+            throw new NoCategoryError("No new category in body.");
+        }
+
+        const newCategory = new Category(reqCategory);
+        const result = await newCategory.save();
+
+        res.status(201).json(result);
+    } catch (err) {
+        if (err instanceof NoCategoryError) {
+            res.status(400).json(err.message);
+        } else {
+            logging.error("Could not create new category.", err.message);
+            res.status(500);
+        }
+    }
+};
+
+export default { getCategories, newCategory };
