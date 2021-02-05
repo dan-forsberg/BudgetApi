0b5df81de5016f076cd5639cf92f737d3c8bff13
Dan Örjestad <danorjest@gmail.com>
2021-02-05 15:18:32 +0100

    removed mongoose

--- source/models/category.ts
+++ source/models/category.ts
@@ -1,8 +1,4 @@
-import mongoose, { Schema, Document } from 'mongoose';
 import ICategory from '../interfaces/category';
 
-const CategorySchema = new Schema({
-    category: { type: String, required: true, trim: true }
-});
 
-export default mongoose.model<Document<ICategory>>('Category', CategorySchema);
+export default {};
--- source/models/entry.ts
+++ source/models/entry.ts
@@ -1,11 +1,6 @@
-import mongoose, { Schema, Document } from 'mongoose';
+
 import IEntry from '../interfaces/entry';
 
-const EntrySchema = new Schema({
-    date: { type: Date, required: true },
-    description: { type: String, required: true, trim: true },
-    amount: { type: Number, required: true },
-    category: { type: mongoose.Types.ObjectId, required: true, ref: 'Category' }
-});
 
-export default mongoose.model<Document<IEntry>>('Entry', EntrySchema);
+
+export default {};
