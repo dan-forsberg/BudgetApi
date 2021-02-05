b423f1ce87490b741c3db8be9d5be444cfe01767
Dan Örjestad <danorjest@gmail.com>
2021-02-05 14:02:14 +0100

    adding entry now works

--- source/controllers/entry.ts
+++ source/controllers/entry.ts
@@ -1,7 +1,56 @@
 import { Request, Response } from 'express';
 import logging from '../config/logging';
+import { ParameterError } from '../interfaces/errors';
 import Entry from '../models/entry';
 
-const workspace = "budget-ctrl"
+const workspace = "entry-ctrl"
 
-export default {};
+const getEntriesForMonth = async (req: Request, res: Response) => {
+    try {
+        const result = await Entry.find();
+        res.status(200).json(result);
+    } catch (err) {
+        logging.error(workspace, "Could not get entries.", err.message);
+        res.status(500);
+    }
+};
+
+const addEntry = async (req: Request, res: Response) => {
+    /*
+    * Request should look like
+    * entries: [{
+    *   date: 2021-03-01 or 2021-03-31, or somewhere in between
+    *   description: "Lön"
+    *   amount: 1000
+    *   category: {id of Dan}
+    * }, ...]
+    */
+    try {
+        const entries = req.body.entries;
+        // check that entries is an array
+        if (!entries || !Array.isArray(entries) || entries.length < 1) {
+            throw new ParameterError("No entries array in object.");
+        }
+
+        // check that entries.date, description, amount and categories exist
+        for (let i = 0; i < entries.length; i++) {
+            const { date, description, amount, category } = entries[i];
+            if (!date || !description || !amount || !category) {
+                throw new ParameterError(
+                    `Missing either parameters in entries[${i}]`);
+            }
+        }
+        const result = await Entry.insertMany(entries);
+        console.log(result);
+        res.status(201).json(result);
+    } catch (err) {
+        if (err instanceof ParameterError) {
+            res.status(400).json(err.message);
+        } else {
+            logging.error(workspace, "Could not add entry.", err.message);
+            res.status(500);
+        }
+    }
+};
+
+export default { getEntriesForMonth, addEntry };
