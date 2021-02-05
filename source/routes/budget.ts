891ae804cdb99a882c64cedd35e16f121129d47c
Dan Örjestad <danorjest@gmail.com>
2021-02-05 07:48:44 +0100

    empty routes

---
+++ source/routes/budget.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import controller from '../controllers/budget';
+
+const router = express.Router();
+//router.get('/get/lists', controller.getAllLists);
+export = router;
---
+++ source/routes/category.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import controller from '../controllers/category';
+
+const router = express.Router();
+//router.get('/get/lists', controller.getAllLists);
+export = router;
