36e1d20fae9f4e98fe18facd9e10d9f68c64126f
Dan Örjestad <danorjest@gmail.com>
2021-02-05 14:01:56 +0100

    budget->entry

--- source/routes/budget.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import controller from '../controllers/budget';
-
-const router = express.Router();
-//router.get('/get/lists', controller.getAllLists);
-export = router;
---
+++ source/routes/entry.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import controller from '../controllers/entry';
+
+const router = express.Router();
+
+router.get('/get', controller.getEntriesForMonth);
+router.post('/new', controller.addEntry);
+
+export = router;
