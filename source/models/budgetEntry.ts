186f932d0cd3aa37ffbe6a8f83d596b8cc5b62f1
Dan Örjestad <danorjest@gmail.com>
2021-02-05 11:49:36 +0100

    category references Category

--- source/models/budgetEntry.ts
+++ source/models/budgetEntry.ts
@@ -5,7 +5,7 @@ const BudgetEntrySchema = new Schema({
     date: { type: Date, required: true },
     description: { type: String, required: true, trim: true },
     amount: { type: Number, required: true },
-    category: { type: mongoose.Types.ObjectId, required: true }
+    category: { type: mongoose.Types.ObjectId, required: true, ref: 'Category' }
 });
 
 export default mongoose.model<Document<IBudgetEntry>>('Entry', BudgetEntrySchema);
