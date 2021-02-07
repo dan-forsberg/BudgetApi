74280679365a5981845518758ed66c4e4befc530
Dan Örjestad <danorjest@gmail.com>
2021-02-07 13:37:53 +0100

    added a unexplicit route for get

--- source/routes/category.ts
+++ source/routes/category.ts
@@ -2,6 +2,7 @@ import express from "express";
 import controller from "../controllers/category";
 
 const router = express.Router();
+router.get("/", controller.getCategories);
 router.get("/get", controller.getCategories);
 router.post("/new", controller.newCategory);
 router.delete("/delete/:id", controller.deleteCategory);
