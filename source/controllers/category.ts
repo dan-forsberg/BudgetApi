77e0cd587d54f6489b9067161ed11c9b6218625f
Dan Örjestad <danorjest@gmail.com>
2021-02-07 07:40:36 +0100

    create, get, delete implemented

--- source/controllers/category.ts
+++ source/controllers/category.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
 import logging from '../config/logging';
 import { Category } from '../models/category';
-import { NoCategoryError } from '../interfaces/errors';
+import { NoCategoryError, ParameterError } from '../interfaces/errors';
+import { ValidationError } from 'sequelize';
 
 const workspace = "category-ctrl"
 
 const getCategories = async (req: Request, res: Response) => {
     try {
-        //let categories = await Category.find();
-        let categories = null;
+        let categories = await Category.findAll(
+            { attributes: ['id', 'category'] }
+        );
         res.status(200).json({ categories: categories });
     } catch (ex) {
         logging.error(workspace, "Could not fetch categories", ex.message);
@@ -18,26 +20,52 @@ const getCategories = async (req: Request, res: Response) => {
 
 const newCategory = async (req: Request, res: Response) => {
     try {
-        const reqCategory = req.body.category;
-        console.log(reqCategory);
-        if (!reqCategory) {
+        const reqCategory = req.body;
+        if (!reqCategory || !reqCategory.category) {
             throw new NoCategoryError("No new category in body.");
         }
 
-        //const newCategory = new Category({ category: reqCategory });
-        //const result = await newCategory.save();
-
-        //console.log(result);
-
-        res.status(201);//.json(result);
+        const result = await Category.create(reqCategory);
+        res.status(201).json({ category: result.category, id: result.id });
     } catch (err) {
         if (err instanceof NoCategoryError) {
             res.status(400).json(err.message);
+        } else if (err instanceof ValidationError) {
+            if (err.name === "SequelizeUniqueConstraintError") {
+                res.status(400).json("Category already exists.");
+            } else {
+                res.status(400).json(err.message);
+            }
         } else {
-            logging.error("Could not create new category.", err.message);
-            res.status(500);
+            res.status(500).json("Could not save category...");
+            logging.error("Could not create new category.", err);
         }
     }
 };
 
-export default { getCategories, newCategory };
+const deleteCategory = async (req: Request, res: Response) => {
+    const categoryToDeleteID = req.params.id;
+    try {
+        if (!categoryToDeleteID || Number.isNaN(categoryToDeleteID)) {
+            throw new ParameterError("No ID specified or ID is NaN.");
+        }
+
+        const result = await Category.destroy({
+            where: {
+                id: categoryToDeleteID
+            }
+        });
+
+        const resCode = (result > 0 ? 200 : 400);
+        res.status(resCode).json({ rowsDeleted: result });
+
+    } catch (err) {
+        if (err instanceof ParameterError) {
+            res.status(400).json(err.message);
+        }
+
+        res.status(500).json({ message: "Something went wrong." });
+    }
+};
+
+export default { getCategories, newCategory, deleteCategory };
--- source/routes/category.ts
+++ source/routes/category.ts
@@ -4,4 +4,5 @@ import controller from '../controllers/category';
 const router = express.Router();
 router.get('/get', controller.getCategories);
 router.post('/new', controller.newCategory);
+router.delete('/delete/:id', controller.deleteCategory);
 export = router;
