ee0dc16bf33ec0fec2e71b16934fbb5b7af558c1
Dan Örjestad <danorjest@gmail.com>
2021-02-07 14:05:29 +0100

    getSpecific -> specific

--- source/routes/entry.ts
+++ source/routes/entry.ts
@@ -4,7 +4,7 @@ import controller from "../controllers/entry";
 const router = express.Router();
 
 router.get("/", controller.getAllEntries);
-router.get("/getSpecific", controller.getSpecific);
+router.get("/specific", controller.getSpecific);
 router.post("/new", controller.addEntry);
 
 export = router;
