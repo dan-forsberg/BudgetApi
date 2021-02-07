65b8e54f69c20de4636781ffa135e7c13ccb565d
Dan Örjestad <danorjest@gmail.com>
2021-02-07 13:35:58 +0100

    added route for specific query

--- source/routes/entry.ts
+++ source/routes/entry.ts
@@ -3,7 +3,8 @@ import controller from "../controllers/entry";
 
 const router = express.Router();
 
-router.get("/get", controller.getEntriesForMonth);
+router.get("/", controller.getAllEntries);
+router.get("/getSpecific", controller.getSpecific);
 router.post("/new", controller.addEntry);
 
 export = router;
