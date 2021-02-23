af9797afc7f09ef91a7247eb21c78212da0df7ae
Dan Örjestad <danorjest@gmail.com>
2021-02-23 14:51:11 +0100

    Better error-handling, start http if https not available

--- source/server.ts
+++ source/server.ts
@@ -1,4 +1,5 @@
 import https from "https";
+import http from "http";
 import express from "express";
 import cors from "cors";
 import fs from "fs";
@@ -6,6 +7,7 @@ import { auth, requiresAuth } from "express-openid-connect";
 
 import logging from "./config/logging";
 import config from "./config/config";
+
 import entryRoutes from "./routes/entry";
 import categoryRoutes from "./routes/category";
 import defaultRoutes from "./routes/defaultEntry";
@@ -18,16 +20,21 @@ import { DefaultEntry } from "./models/defaultEntry";
 const NAMESPACE = "Server";
 const router = express();
 
-const letsEncrypt = process.env.letsEncrypt;
-const privateKey = fs.readFileSync(`${letsEncrypt}/privkey.pem`, "utf8");
-const certificate = fs.readFileSync(`${letsEncrypt}//cert.pem`, "utf8");
-const ca = fs.readFileSync(`${letsEncrypt}//chain.pem`, "utf8");
+let credentials: { key: string, cert: string, ca: string; } | undefined = undefined;
+try {
+	const letsEncrypt = process.env.letsEncrypt;
+	const privateKey = fs.readFileSync(`${letsEncrypt}/privkey.pem`, "utf8");
+	const certificate = fs.readFileSync(`${letsEncrypt}//cert.pem`, "utf8");
+	const ca = fs.readFileSync(`${letsEncrypt}//chain.pem`, "utf8");
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+	credentials = {
+		key: privateKey,
+		cert: certificate,
+		ca: ca
+	};
+} catch (err) {
+	logging.error(NAMESPACE, "Could not find LE-files or .env. Starting server as HTTP!\n", err.message);
+}
 
 const authConfig = {
 	authRequired: false,
@@ -38,42 +45,39 @@ const authConfig = {
 	issuerBaseURL: process.env.issuerBaseURL
 };
 
-
-/** Connect to MariaDB */
 MariaDB.authenticate().then(() => {
 	logging.info(NAMESPACE, "MariaDB connected successfully!");
 	Category.sync().then(() => {
 		Entry.sync();
 		DefaultEntry.sync();
 	});
+}).catch((err) => {
+	logging.error(NAMESPACE, "Could not connect to MariaDB. Exiting.", err.message);
+	process.exit(1);
 });
-/** Do not catch, let the server crash and burn terribly. */
 
-/** Authentication */
-router.use(auth(authConfig));
-
+try {
+	router.use(auth(authConfig));
+} catch (err) {
+	logging.error(NAMESPACE, "Could not start authentication. Exiting.", err.message);
+	process.exit(1);
+}
+router.use(cors());
 
 /** Log the request */
 router.use((req, res, next) => {
-	/** Log the req */
 	logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
 	res.on("finish", () => {
-		/** Log the res */
 		logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
 	});
 
 	next();
 });
 
-router.use(cors());
-
-
-/** Parse the body of the request */
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
-/** Rules of our API */
 router.use("/api", (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -103,6 +107,12 @@ router.use("*", (_, res) => {
 	});
 });
 
-const httpsServer = https.createServer(credentials, router);
-
-httpsServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running https://${config.server.hostname}:${config.server.port}`));
+if (credentials !== undefined) {
+	const httpsServer = https.createServer(credentials, router);
+	httpsServer.listen(config.server.httpsPort, () => logging.info(NAMESPACE, `Server is running https://${config.server.hostname}:${config.server.httpsPort}`));
+	// TODO: add a http server to redirect to HTTPS
+} else {
+	logging.info(NAMESPACE, "Starting HTTP-server, but no HTTPS!");
+	const httpServer = http.createServer(router);
+	httpServer.listen(config.server.httpPort, () => logging.info(NAMESPACE, `Server is running http://${config.server.hostname}:${config.server.httpPort}`));
+}
