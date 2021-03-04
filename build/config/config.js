4f0d9a396bc2ac61158d5d25e601ad2f14932270
Dan Örjestad <danorjest@gmail.com>
2021-03-04 07:53:14 +0100

    Rebuild

--- build/config/config.js
+++ build/config/config.js
@@ -1,21 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var sql = {
-    database: "economy",
+    database: process.env.db || "economy",
     username: process.env.dbUser || "economy",
     password: process.env.dbPassword || "economy",
-    host: "localhost",
-    port: 3306,
+    host: process.env.dbHost || "0.0.0.0",
+    port: process.env.dbPort || 3306,
     dialect: "mariadb",
     // if dbLogging is set, logg, otherwise don't log any queries 
     logging: process.env.dbLogging !== undefined,
 };
 var SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "0.0.0.0";
-var SERVER_HTTPS_PORT = process.env.SERVER_HTTPS_PORT || 4433;
 var SERVER_HTTP_PORT = process.env.SERVER_HTTP_PORT || 8080;
 var SERVER = {
     hostname: SERVER_HOSTNAME,
-    httpsPort: SERVER_HTTPS_PORT,
     httpPort: SERVER_HTTP_PORT
 };
 var config = {
