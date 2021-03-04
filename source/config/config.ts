101864abd9d7fa7f4ec59578bd0d43fd46ddccba
Dan Örjestad <danorjest@gmail.com>
2021-03-04 07:49:52 +0100

    Updated config to remove https and use env vars for db

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,21 +1,19 @@
 const sql = {
-	database: "economy",
+	database: process.env.db || "economy",
 	username: process.env.dbUser || "economy",
 	password: process.env.dbPassword || "economy",
-	host: "localhost",
-	port: 3306,
+	host: process.env.dbHost || "0.0.0.0",
+	port: process.env.dbPort || 3306,
 	dialect: "mariadb",
 	// if dbLogging is set, logg, otherwise don't log any queries 
 	logging: process.env.dbLogging !== undefined,
 };
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "0.0.0.0";
-const SERVER_HTTPS_PORT = process.env.SERVER_HTTPS_PORT || 4433;
 const SERVER_HTTP_PORT = process.env.SERVER_HTTP_PORT || 8080;
 
 const SERVER = {
 	hostname: SERVER_HOSTNAME,
-	httpsPort: SERVER_HTTPS_PORT,
 	httpPort: SERVER_HTTP_PORT
 };
 
