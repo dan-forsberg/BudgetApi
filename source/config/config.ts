7709910ff37a295e7411ebd2f21c66a55a926ad1
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-03-07 13:54:18 +0000

    Use env vars instead of hard-coded values

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,9 +1,9 @@
 const sql = {
-	database: "economy",
-	username: process.env.dbUser || "economy",
-	password: process.env.dbPassword || "economy",
-	host: "localhost",
-	port: 3306,
+	database: process.env.DB_DATABASE || "economy",
+	username: process.env.DB_USER     || "economy",
+	password: process.env.DB_PASSWORD || "economy",
+	host:  process.env.DB_HOST        || "localhost",
+	port: process.env.DB_PORT         || 3306,
 	dialect: "mariadb",
 	// if dbLogging is set, logg, otherwise don't log any queries 
 	logging: process.env.dbLogging !== undefined,
@@ -11,12 +11,10 @@ const sql = {
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "0.0.0.0";
 const SERVER_HTTPS_PORT = process.env.SERVER_HTTPS_PORT || 4433;
-const SERVER_HTTP_PORT = process.env.SERVER_HTTP_PORT || 8080;
 
 const SERVER = {
 	hostname: SERVER_HOSTNAME,
 	httpsPort: SERVER_HTTPS_PORT,
-	httpPort: SERVER_HTTP_PORT
 };
 
 const config = {
