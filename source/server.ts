79f54978f7d304119dcf14fb0b6a44ec399f3fba
Dan Örjestad <danorjest@gmail.com>
2021-03-18 08:06:24 +0100

    remove commented block

--- source/server.ts
+++ source/server.ts
@@ -49,18 +49,6 @@ router.use((req, res, next) => {
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
-// router.use("/api", (req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-// 	if (req.method == "OPTIONS") {
-// 		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-// 		return res.status(200).json({});
-// 	}
-
-// 	next();
-// });
-
 if (production) {
 	const authConfig = {
 		"domain": "dev-dasifor.eu.auth0.com",
